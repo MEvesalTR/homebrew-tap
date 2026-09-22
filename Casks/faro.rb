3420972a43767dc835c661c7418206a960449d08
Mohammad Erfan Vesaltalab <envb.2002@gmail.com>
2026-09-22 10:21:51 +0330

    chore: update faro cask to version 0.0.4

--- Casks/faro.rb
+++ Casks/faro.rb
@@ -1,13 +1,13 @@
 cask "faro" do
-  version "0.0.3"
+  version "0.0.4"
 
   on_arm do
-    sha256 "62ba0c3667c45ea1dbfe49cca93a8cfe04b2d1d37fbccd3f269ee6353a3cddc4"
+    sha256 "7159185f0c4833b9ea21350da1c244a3c1ca103e3dbd1a2c3f31f7bc6f678d7b"
     url "https://s3.ir-tbz-sh1.arvanstorage.ir/hamafzar/releases/avapardaz/faro-flutter/macos/#{version}/arm64/Faro-v#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "ce5b28d7ccc89f3e713ea6e48ec009d45081e014f00fdda05c2480c99884f7db"
+    sha256 "7a261d6a536c22944613b52a5a82fc80c15cb28f4f2fcff65e1307c9649d8570"
     url "https://s3.ir-tbz-sh1.arvanstorage.ir/hamafzar/releases/avapardaz/faro-flutter/macos/#{version}/x64/Faro-v#{version}-x64.dmg"
   end
 
