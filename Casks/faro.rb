8b0305e9bc5dc7fece9c8e2c6c96a798f1b7f93f
Mohammad Erfan Vesaltalab <envb.2002@gmail.com>
2026-09-22 10:55:07 +0330

    chore: update Faro cask with macOS dependency, postflight steps, and zap paths

--- Casks/faro.rb
+++ Casks/faro.rb
@@ -3,22 +3,34 @@ cask "faro" do
 
   on_arm do
     sha256 "7159185f0c4833b9ea21350da1c244a3c1ca103e3dbd1a2c3f31f7bc6f678d7b"
+
     url "https://s3.ir-tbz-sh1.arvanstorage.ir/hamafzar/releases/avapardaz/faro-flutter/macos/#{version}/arm64/Faro-v#{version}-arm64.dmg"
   end
-
   on_intel do
     sha256 "7a261d6a536c22944613b52a5a82fc80c15cb28f4f2fcff65e1307c9649d8570"
+
     url "https://s3.ir-tbz-sh1.arvanstorage.ir/hamafzar/releases/avapardaz/faro-flutter/macos/#{version}/x64/Faro-v#{version}-x64.dmg"
   end
 
   name "Faro"
   desc "Official Desktop Application for Faro"
-  homepage "https://faro.ir"
+  homepage "https://faro.ir/"
+
+  depends_on macos: :monterey
 
   app "Faro.app"
 
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-cr", "{{appdir}}/Faro.app"]
+  end
+
   zap trash: [
+    "~/Library/Application Scripts/ir.avapardaz.faro",
     "~/Library/Application Support/faro",
-    "~/Library/Preferences/com.example.faro.plist",
+    "~/Library/Application Support/ir.avapardaz.faro",
+    "~/Library/Caches/ir.avapardaz.faro",
+    "~/Library/Containers/ir.avapardaz.faro",
+    "~/Library/Preferences/ir.avapardaz.faro.plist",
+    "~/Library/Saved Application State/ir.avapardaz.faro.savedState",
   ]
 end
